284da5c4017d60e537dbc587013e57a9789b90c0
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-05-18 16:59:00 -0400

    Added Bootstrap React

--- air-rnv-client/src/App.js
+++ air-rnv-client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Alert, Breadcrumb } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Breadcrumb>
+        <Breadcrumb.Item>Test</Breadcrumb.Item>
+      </Breadcrumb>
+        <Alert variant="success">This is a button</Alert>
+        <Button>Test Button</Button>
     </div>
   );
 }
