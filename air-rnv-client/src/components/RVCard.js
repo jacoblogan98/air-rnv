2b41764bc7e5c1cf68c46035b627a7cde85d165a
Elijah Smith <elijahsmith.m@gmail.com>
2022-05-19 00:35:15 -0500

    used data

--- air-rnv-client/src/components/RVCard.js
+++ air-rnv-client/src/components/RVCard.js
@@ -9,6 +9,7 @@ function RVCard({ rv }) {
     year,
     summary,
     length,
+    rv_class,
     dayRate,
     petFriendly,
     airConditioned,
@@ -21,7 +22,16 @@ function RVCard({ rv }) {
     <Card style={{ width: "18rem" }}>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Image>{image_url}</Card.Image>
+        <Card.Text>{length}</Card.Text>
+        <Card.Text>{`Pet friendly: ${
+          petFriendly === true ? "Yes!" : "No..."
+        }`}</Card.Text>
+        <Card.Text>{rv_class}</Card.Text>
+        <Card.Text>{year}</Card.Text>
+        <Card.Text>{shower}</Card.Text>
+        <Card.Text>{tv}</Card.Text>
+
+        {/* <Card.Image></Card.Image> */}
         <Card.Subtitle className="mb-2 text-muted">{occupancy}</Card.Subtitle>
         <Card.Text>{summary}</Card.Text>
         {/* <Card.Link href="">Card Link</Card.Link>
--- air-rnv-client/src/pages/Home.js
+++ air-rnv-client/src/pages/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-// import RVCard from "../components/RVCard";
+import RVCard from "../components/RVCard";
 // import Card from "react-bootstrap/Card";
 import RvList from "./RvList";
 
 function Home({ rvList }) {
-  //   const renderRvs = rvList.map((rv) => {
-  //     console.log(rv);
-  //     return <RVCard key={rv.id} rv={rv} />;
-  //   });
+  const renderRvs = rvList.map((rv) => {
+    console.log(rv);
+    return <RVCard key={rv.id} rv={rv} />;
+  });
   return (
     <div>
       <RvList rvList={rvList} />
-      {/* {renderRvs} */}
+      {renderRvs}
     </div>
   );
 }
