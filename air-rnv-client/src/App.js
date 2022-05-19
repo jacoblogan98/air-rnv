a0a0ad0dfcaf01e89cada4e97819da946001b1d6
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-05-18 22:32:49 -0400

    Added Sass for modifying color theme

--- air-rnv-client/src/App.js
+++ air-rnv-client/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
 import { Button, Alert, Breadcrumb } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <Breadcrumb>
-        <Breadcrumb.Item>Test</Breadcrumb.Item>
+      <Breadcrumb variant='rosewood'>
+        <Breadcrumb.Item >Test</Breadcrumb.Item>
       </Breadcrumb>
-        <Alert variant="success">This is a button</Alert>
-        <Button>Test Button</Button>
+        <Alert variant='rosewood'>This is a button</Alert>
+        <Button variant='maple' className='m-2' id='btn-maple'>Button</Button>
+        <Button variant='olive' className='m-2' id="btn-olive">Button</Button>
+        <Button variant="rosewood" className='m-2' id='btn-rosewood'>Button</Button>
+        <Button variant="cherry" className='m-2' id='btn-cherry'>Button</Button>
+        <Button variant="sand" className='m-2' id='btn-sand'>Button</Button>
     </div>
   );
 }
