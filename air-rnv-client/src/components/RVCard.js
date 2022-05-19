b9f68b4542e46c5fa5985e4a9d9390c6c36dff62
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-05-19 17:34:49 -0400

    Added images and styling for cards

--- air-rnv-client/src/components/RVCard.js
+++ air-rnv-client/src/components/RVCard.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+
 
 function RVCard({ rv }) {
   const {
@@ -29,24 +33,49 @@ function RVCard({ rv }) {
     };
 
   return (
-    <Card style={{ width: "18rem" }} onClick={() => showDetails(id)} role="button" >
-      <Card.Body>
-        <Card.Title>The {name}.</Card.Title>
-        <Card.Text>Rate: ${day_rate} /night</Card.Text>
-        <Card.Text>Location: {location}</Card.Text>
-        <Card.Text>Sleeps: {occupancy}</Card.Text>
-        <Card.Text>Length: {length} ft</Card.Text>
-        <Card.Text>Class: {rv_class}</Card.Text>
-        <Card.Text>Year: {year}</Card.Text>
-        <Card.Text>{pet_friendly ? <i class="fa fa-paw" aria-hidden="true" /> : null}</Card.Text>
-        <Card.Text>{shower ? <i class="fa fa-shower" aria-hidden="true" /> : null}</Card.Text>
-        <Card.Text>{tv ? <i class="fa fa-television" aria-hidden="true" /> : null}</Card.Text>
-        <Card.Text>{air_conditioned ? <i class="fa fa-snowflake-o" aria-hidden="true" /> : null}</Card.Text>
-        <Card.Subtitle className="mb-2 text-muted">{description}</Card.Subtitle>
-        {/* <Card.Link href="">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link> */}
-      </Card.Body>
-    </Card>
+    <Col >
+      <Card
+        onClick={() => showDetails(id)} 
+        role="button" 
+        >
+        <Card.Body>
+          <Card.Img 
+          variant="top" 
+          src="https://source.unsplash.com/random/500x500" 
+          className="mb-3"
+          />
+          <Card.Title>The {name}.</Card.Title>
+          <Card.Text>Rate: ${day_rate} /night</Card.Text>
+          <Card.Text>Location: {location}</Card.Text>
+          <Card.Text>Sleeps: {occupancy}</Card.Text>
+          <Card.Text>Length: {length} ft</Card.Text>
+          <Card.Text>Class: {rv_class}</Card.Text>
+          <Card.Text>Year: {year}</Card.Text>
+          
+          <Row >
+            <Col>
+              <Card.Text>{pet_friendly ? <i class="fa fa-paw" aria-hidden="true" /> : <i class="fa fa-paw" aria-hidden="true" style={{display: "none"}}/>}
+              </Card.Text>
+            </Col>
+            <Col>
+              <Card.Text>{shower ? <i class="fa fa-shower" aria-hidden="true" /> : <i class="fa fa-shower" aria-hidden="true" style={{display: "none"}}/>}</Card.Text>  
+            </Col>
+            <Col>
+              <Card.Text>{tv ? <i class="fa fa-television" aria-hidden="true" /> : null}</Card.Text>
+            </Col>
+            <Col>
+              <Card.Text>{air_conditioned ? <i class="fa fa-snowflake-o" aria-hidden="true" /> : null}</Card.Text>
+            </Col>
+          </Row>
+          
+
+          <Card.Subtitle className="mb-2 text-muted">{description}</Card.Subtitle>
+          {/* <Card.Link href="">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link> */}
+        </Card.Body>
+      </Card>
+    </Col>
+    
   );
 }
 
