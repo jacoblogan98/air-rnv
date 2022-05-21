2ebb485e4a316bbfbae6b799894555814278d27e
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-05-20 21:20:43 -0400

    Made cards for reviews and pruned Details

--- air-rnv-client/src/pages/Details.js
+++ air-rnv-client/src/pages/Details.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ImageCarousel from "../components/ImageCarousel";
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 
 function Details() {
@@ -19,23 +23,19 @@ function Details() {
   }, [params.id])
 
   const {
-    air_conditioned,
     photos,
-    created_at,
-    day_rate,
-    description,
-    id,
-    length,
-    location,
     name,
+    location,
+    day_rate,
     occupancy,
+    description,
+    air_conditioned,
     pet_friendly,
-    rv_class,
     shower,
     tv,
-    updated_at,
-    user_id,
     year,
+    length,
+    rv_class,
     reviews
   } = rvDetails;
 
@@ -45,47 +45,55 @@ function Details() {
     const Arr = []
 
     for (let i = review.rating; i > 0; i--) {
-      Arr.push("fa fa-star checked")
+      Arr.push("fa fa-star checked text-maple")
     }
     
     for (let i = (5 - review.rating); i > 0; i--) {
-      Arr.push("fa fa-star-o")
+      Arr.push("fa fa-star-o text-maple")
     }
 
     const renderStars = Arr.map((starClass, i) => <span key={i} className={starClass}></span>)
 
     return (
-      <ul key={review.id}> 
-        <li>{renderStars}</li>
-        <li>By: {review.user_name}</li>
-        <li>{review.content}</li>
-      </ul>
+      <Col>
+        <Card
+        key={review.id}
+        style={{ width: '18rem' }}
+        className="mb-2"
+        >
+          <Card.Header className="bg-sand">{renderStars}</Card.Header>
+          <Card.Body>
+            <Card.Title>{review.user_name}</Card.Title>
+            <Card.Text>{review.content}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      
     )
   })
   
   return (
     <div>
       <h2 className="bubble-font ">{name}</h2>
-      <ul>
-        {renderReviews}
-      </ul>
       <ImageCarousel photos={photos} />
-      <h4>Name: {name}</h4>
-      <h4>Length: {length}</h4>
-      <h4>Day Rate: {day_rate}</h4>
-      <h4>Description: {description}</h4>
-      <h4>Air Conditioned{air_conditioned}</h4>
-      <h4>Created At: {created_at}</h4>
-      <h4>ID: {id}</h4>
       <h4>Location: {location}</h4>
+      <h4>Day Rate: {day_rate}</h4>
       <h4>Occupancy: {occupancy}</h4>
+      <h4>Description: {description}</h4>
+
+      <h4>Air Conditioned{air_conditioned}</h4>
       <h4>Pet Friendly: {pet_friendly}</h4>
-      <h4>RV Class: {rv_class}</h4>
       <h4>Shower: {shower}</h4>
       <h4>TV: {tv}</h4>
-      <h4>Updated At: {updated_at}</h4>
-      <h4>User_id: {user_id}</h4>
+      
       <h4>Year: {year}</h4>
+      <h4>Length: {length}</h4>
+      <h4>RV Class: {rv_class}</h4>
+      <Container className="mb-4">
+        <Row className="justify-content-around">
+          {renderReviews}
+        </Row>
+      </Container>
     </div>
   );
 }
